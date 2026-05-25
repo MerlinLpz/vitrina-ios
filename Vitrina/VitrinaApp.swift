ca1e319d8c35d7493e1d7d53b4aea1b599e5313a
Merlin <lopezponcemerlin@gmail.com>
2026-05-25 14:19:40 +0200

    Initial Commit

---
+++ Vitrina/VitrinaApp.swift
@@ -0,0 +1,17 @@
+//
+//  VitrinaApp.swift
+//  Vitrina
+//
+//  Created by mierlin on 25/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct VitrinaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
